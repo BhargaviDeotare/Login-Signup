28a90e701f0db5c81ac7157fea55f0a6fc794ebd
bhargavi <deotarebhargavi@gmail.com>
2023-01-21 22:23:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/home/home";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+import { Router, Route, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <BrowserRouter>
+          <Route exact path="/">
+            {user && user._id ? (
+              <Home setLoginUser={setLoginUser} />
+            ) : (
+              <Login setLoginUser={setLoginUser} />
+            )}
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </BrowserRouter>
+      </Router>
     </div>
   );
 }
